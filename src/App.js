34ebb600246cb1b1f4a4aa106899e1519c5f8259
Bob_chemist <bob_chemist@mail.ru>
2019-05-07 08:24:00 +0300

    Searching by submitting

--- src/App.js
+++ src/App.js
@@ -3,29 +3,34 @@ import axios from 'axios';
 
 function App() {
   const [results, setResults] = useState([]);
+  const [query, setQuery] = useState('reacthooks');
 
   useEffect(() => {
     getData();
-  }, []);
+    // eslint-disable-next-line
+  }, [query]);
   console.log(results);
 
   const getData = async () => {
     const response = await axios.get(
-      'http://hn.algolia.com/api/v1/search?query=reacthooks'
+      `http://hn.algolia.com/api/v1/search?query=${query}`
     );
     setResults(response.data.hits);
   };
 
   return (
-    <ol>
-      {results.map(item => {
-        return (
-          <li key={item.objectID}>
-            <a href={item.url}>{item.title}</a>
-          </li>
-        );
-      })}
-    </ol>
+    <>
+      <input type="text" onChange={event => setQuery(event.target.value)} />
+      <ol>
+        {results.map(item => {
+          return (
+            <li key={item.objectID}>
+              <a href={item.url}>{item.title}</a>
+            </li>
+          );
+        })}
+      </ol>
+    </>
   );
 }
 
